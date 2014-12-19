1ce15ab2fc16acdc507c8cecc52271dfd2ed6afd
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-12-19 09:58:56 -0700

    created contact us page

---
+++ src/htdocs/contact.php
@@ -0,0 +1,209 @@
+<?php
+	if(!isset($TEMPLATE)) {
+		$TEMPLATE = 'onecolumn';
+		$TITLE = "Contact Us";
+		include 'template.inc.php';
+	}
+?>
+
+<div class="row">
+  <div class="one-of-two column">
+    <h2>Staff at Headquarters</h2>
+    <ul class="no-style">
+      <li class="row">
+        <img src="/images/staff/finn_a_sm.jpg" alt=""
+            class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Carol Finn<br/>
+          Geomagnetism Group Leader<br/>
+          Ph: (303) 273-8475<br/>
+          <a href="mailto:cafinn@usgs.gov">cafinn@usgs.gov</a>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Love_Jeff.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          <a href="http://profile.usgs.gov/jlove">Jeffrey J. Love</a><br/>
+          USGS Advisor for Geomagnetic Research<br/>
+          Ph: (303) 273-8540<br/>
+          <a href="mailto:jlove@usgs.gov">jlove@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Caldwell_Jill.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Jill E. Caldwell<br/>
+          Data Processing<br/>
+          Ph: (303) 273-8486<br/>
+          <a href="mailto:jcaldwell@usgs.gov">jcaldwell@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Jeff_Fox_sm.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Jeffrey L. Fox<br/>
+          Operations<br/>
+          Ph: (303) 273-8659<br/>
+          <a href="mailto:jfox@usgs.gov">jfox@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/McWhirter_Eddie.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Edward A. McWhirter Jr.<br/>
+          Software Development<br/>
+          Ph: (303) 273-8488<br/>
+          <a href="mailto:emcwhirter@usgs.gov">emcwhirter@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Morris_Jake_small.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Jake R. Morris<br/>
+          Operations<br/>
+          Ph: (303) 273-8603<br/>
+          <a href="mailto:jakemor@usgs.gov">jakemor@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Nelson_Mark.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Mark Nelson<br/>
+          Facilities Manager<br/>
+          Ph: (303) 273-8483<br/>
+          <a href="mailto:mdnelson@usgs.gov">mdnelson@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Rigler_Josh_small.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          E. Joshua 'Josh' Rigler<br/>
+          Research and Development<br/>
+          Ph: (303) 273-8607<br/>
+          <a href="mailto:erigler@usgs.gov">erigler@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Sauter_Ed.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Edward A. Sauter<br/>
+          Electronics<br/>
+          Ph: (303) 273-8471<br/>
+          <a href="mailto:esauter@usgs.gov">esauter@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Simpson_Hal.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Hal A. Simpson<br/>
+          Software Development<br/>
+          <a href="mailto:hasimpson@usgs.gov">hasimpson@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/Duff02.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Duff C. Stewart<br/>
+          Software Development<br/>
+          Ph: (303) 273-8485<br/>
+          <a href="mailto:dcstewart@usgs.gov">dcstewart@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/White_Tim.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          Tim C. White<br/>
+          Electronics<br/>
+          Ph: (303) 273-8409<br/>
+          <a href="mailto:ttwhite@usgs.gov">ttwhite@usgs.gov</a><br/>
+        </div>
+      </li>
+      <hr>
+      <li class="row">
+        <img src="/images/staff/bill2_sm.jpg" alt="" class="column mobile-one-of-four" />
+        <div class="column mobile-three-of-four">
+          E. William 'Bill' Worthington<br/>
+          Operations<br/>
+          Ph: (303) 273-8472<br/>
+          <a href="mailto:bworth@usgs.gov">bworth@usgs.gov</a><br/>
+        </div>
+      </li>
+    </ul>
+    <h2>Staff Outside of Headquarters</h2>
+    <ul class="no-style">
+    <li class="row">
+      <img src="/images/staff/Murray_Eric_small.jpg" alt="" class="column mobile-one-of-four" />
+      <div class="column mobile-three-of-four">
+        Eric V. Murray<br/>
+        Fredericksburg Operations<br/>
+        Ph: (540) 373-7601<br/>
+        <a href="mailto:emurray@usgs.gov">emurray@usgs.gov</a><br/>
+      </div>
+    </li>
+    </ul>
+  </div>
+  <div class="one-of-two column">
+    <h2>Observatory Operations</h2>
+    Carol A. Finn<br/><a href="mailto:cafinn@usgs.gov">cafinn@usgs.gov</a>
+
+    <h2>Website Issues</h2>
+    Eddie McWhirter<br/>
+    <a href="mailto:emcwhirter@usgs.gov?cc=lisa@usgs.gov">
+        emcwhirter@usgs.gov
+    </a>
+
+    <h2>Headquarters</h2>
+    <dl>
+      <dt>Mailing Address</dt>
+        <dd>
+          USGS DFC<br/>
+          Box 25046, MS 966<br/>
+          Denver, CO 80225-0046</br>
+        </dd>
+      <dt>Physical/Delivery Address</dt>
+        <dd>
+          USGS</br>
+          1711 Illinois Street<br/>
+          Golden, CO 80401-1865<br/>
+        </dd>
+      <dt>Fax</dt>
+        <dd>
+          (303) 273-8506
+        </dd>
+    </dl>
+
+    <h2>College Observatory</h2>
+    <dl>
+      <dt>Address</dt>
+        <dd>
+          PO Box 756920<br/>
+          Fairbanks, AK 99775-6920<br/>
+        </dd>
+    </dl>
+
+    <h2>Fredericksburg</h2>
+    <dl>
+      <dt>Mailing Address</dt>
+        <dd>
+          PO Box 190<br/>
+          Corbin, VA 22446<br/>
+        </dd>
+      <dt>Physical/Delivery Address</dt>
+        <dd>
+          15351 Office Drive<br/>
+          Corbin, VA 22446<br/>
+        </dd>
+    </dl>
+  </div>
+</div>
