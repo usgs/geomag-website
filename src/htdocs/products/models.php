6944c2546fbc486e3781f9a87aa9c9ba43fba596
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-12-22 15:41:46 -0700

    set up models page

---
+++ src/htdocs/products/models.php
@@ -0,0 +1,25 @@
+<?php
+  if(!isset($TEMPLATE)) {
+    $TITLE = 'Models';
+    include 'template.inc.php';
+  }
+?>
+
+<p>
+  For main-field models and calculators for declination and other field
+  components, please visit
+  <a href='http://www.ngdc.noaa.gov/geomag/magfield.shtml'>NOAA</a>.
+</p>
+
+<p>
+  <a href="/publications/movies/">Magnetic Disturbance Movies</a>
+</p>
+
+<p>
+  <a href="/conductivity/">Conductivity Map</a>
+</p>
+
+<p>
+  <a href="movies/index.php?type=declination&format=gif">
+  Declination of the Earth 1590-1990</a>
+</p>
