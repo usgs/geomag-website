5b089ecb9d24add6d9bed6ae2f33d99fd6331272
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-12-22 15:42:10 -0700

    set up other data page

---
+++ src/htdocs/products/otherdata.php
@@ -0,0 +1,69 @@
+<?php
+  if(!isset($TEMPLATE)) {
+    $TITLE = 'Other Sources of Data';
+    include 'template.inc.php';
+  }
+?>
+<div class="row">
+  <div class="one-of-two column">
+    <dl>
+      <dt>Variational Array Programs</dt>
+        <dd>
+          <a href="http://space.augsburg.edu/ago/index.html">
+              AGO Antarctica</a><br />
+          <a href="http://www.space.dtu.dk/English/Research/Scientific_data_and_models/Magnetic_Ground_Stations.aspx">
+              DMI Greenland</a><br />
+          <a href="http://www.asf.alaska.edu/program/gdc/project/magnetometer">
+              GIMA Alaska</a><br />
+          <a href="http://sprg.ssl.berkeley.edu/UCLANLMAG/">
+              IGPP/LANL Western US</a>
+          <a href="http://www.geo.fmi.fi/image/">IMAGE Northern Europe</a><br />
+          <a href="http://space.engin.umich.edu">Measure Eastern US</a><br />
+          <a href="http://flux.phys.uit.no/geomag.html">
+              Troms&oslash; Geophysical Observatory</a><br />
+          <a href="http://www.dcs.lancs.ac.uk/iono/samnet/">SAMNET</a><br />
+          <a href="http://www-ssc.igpp.ucla.edu/uclamag/SMALL/">SMALL</a><br />
+          <a href="http://www.irf.se/mag/">China</a><br />
+          <a href="http://stdb2.stelab.nagoya-u.ac.jp/mm210/index.html">
+              210 Magnetic Meridian</a>
+        </dd>
+    </dl>
+  </div>
+  <div class="one-of-two column">
+    <dl>
+      <dt>Alternate Sources of Geomagnetic Data</dt>
+        <dd>
+          <a href="http://www.ngdc.noaa.gov">
+              National Geophysical Data Center (NOAA)</a><br />
+          <a href="http://nssdc.gsfc.nasa.gov/">
+              National Space Science Data Center (NASA)</a><br />
+          <a href="http://www.space.dtu.dk/English/Research/Scientific_data_and_models/World_Data_Center_for_Geomagnetism.aspx">
+              World Data Center Copenhagen</a><br />
+          <a href="http://swdcwww.kugi.kyoto-u.ac.jp/index.html">
+              World Data Center Kyoto</a><br />
+          <a href="http://www.intermagnet.org">Intermagnet</a>
+        </dd>
+      <dt>Satellite Programs</dt>
+        <dd>
+          <a href="http://op.gfz-potsdam.de/champ/index_CHAMP.html">
+              Champ</a><br />
+          <a href="http://dmsp.ngdc.noaa.gov/dmsp.html">
+              Defense Meteorological Satellite Program</a><br />
+          <a href="http://web.dmi.dk/fsweb/projects/oersted/">Oersted</a><br />
+        </dd>
+      <dt>Looking for your link?</dt>
+        <dd>
+          <div class="row">
+            <img src="/images/compass1.gif" alt="Picture of a Compass"
+                class="column mobile-one-of-four"/>
+            <div class="column mobile-three-of-four">
+              <b>Contact:</b><br />
+              Carol A. Finn<br />
+              Geomagnetism Group Leader<br />
+              <a href="mailto:cafinn@usgs.gov">cafinn@usgs.gov</a>
+            </div>
+          </div>
+        </dd>
+    </dl>
+  </div>
+</div>
