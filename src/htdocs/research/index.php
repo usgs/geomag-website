a58667c9e609b7a20bdfc1535142057a08cc170b
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-12-23 13:07:51 -0700

    set up index page

---
+++ src/htdocs/research/index.php
@@ -0,0 +1,128 @@
+<?php
+
+if (!isset($TEMPLATE)) {
+  $TITLE = 'Research';
+  $HEAD = '<link rel="stylesheet" href="index.scss"/>';
+  include 'template.inc.php';
+}
+
+?>
+
+<ul class="row two-up no-style">
+  <li class="column">
+    <h2><a href="/publications/papers.php">Publications</a></h2>
+    <div class="row">
+      <img src="/images/publications.jpg" alt=""
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="geomagnetism-earthquakes.php">Earthquake Prediction</a>
+        </h2>
+    <div class="row">
+      <img src="/images/earthquake-icon.jpg" alt=""
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="GIC.php">Geomagnetically Induced Currents</a></h2>
+    <div class="row">
+      <img src="/images/dreamstime_l_4671381_small.jpg" alt="Used with permission from dreamstime.com"
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="hazards.php">Hazard Mapping</a></h2>
+    <div class="row">
+      <img src="/images/hazardmapping_150_100.png" alt=""
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="magdataquality.php">Magnetic Data Quality</a></h2>
+    <div class="row">
+      <img src="/images/Data-icon.jpg" alt=""
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="extreme-events.php">Extreme Events</a></h2>
+    <div class="row">
+      <img src="/images/Extreme-icon-2.jpg" alt=""
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="/conductivity/">Regional Conductivity Map</a></h2>
+    <div class="row">
+      <img src="/images/locations2_150_100.png" alt=""
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="spaceweather.php">Space Weather Applications</a></h2>
+    <div class="row">
+      <img src="/images/Aurora.jpg" alt=""
+          class="column mobile-one-of-four" />
+      <!-- <p class="column mobile-three-of-four">
+
+      </p> -->
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="spaceclimatology.php">Space Climatology</a></h2>
+    <div class="row">
+      <img src="/images/Climatology-icon.jpg" alt=""
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+  <li class="column">
+    <h2><a href="/publications/movies/">Movies</a></h2>
+    <div class="row">
+      <img src="/images/dreamstime_xs_10405769_small.jpg" alt="Used with permission from dreamstime.com"
+          class="column mobile-one-of-four" />
+    </div>
+  </li>
+</ul>
+
+<p>
+  Research projects within the USGS Geomagnetism Program are targeted for
+  societal relevance, especially for space-weather hazard science. Consistent
+  with the goals and issues addressed in the USGS Natural Hazards Strategic
+  Plan, these research projects support pursuit of fundamental understanding of
+  geomagnetic storms and related phenomena, development of geomagnetic products
+  needed for assessments and real-time situational awareness, informed operation
+  of the Program's magnetic-observatory network. Geomagnetism Program research
+  staff provide outside advice, national and international leadership, and write
+  educational articles about geomagnetic and space-weather hazards.
+</p>
+
+<p>
+  Fundamental research within the USGS Geomagnetism Program is concentrated on
+  the analysis of the information content of magnetic-field time series acquired
+  at ground-base magnetic observatories. Methods include formal time-series and
+  statistical analysis, and data assimilation for physics-based models of the
+  magnetosphere and ionosphere. Recent work has included the analysis of the
+  symmetry (and asymmetry) of low-latitude ground-level magnetic disturbance
+  during individual magnetic storms; long-term secular change in geomagnetic
+  disturbance and its correlational relationship with sunspots and
+  interplanetary solar wind; secular change in geomagnetic activity and global
+  climate change; solar-quiet geomagnetic variation and periodic geomagnetic
+  tides; extreme-geophysical-event analysis; and investigation of claims that
+  earthquakes are sometimes preceded by magnetic precursory signals.
+</p>
+
+<p>
+  Guided by fundamental understanding, geomagnetic products are developed to
+  meet the needs of the space-weather community for hazard assessments and
+  real-time situational awareness. Recent work has concentrated on the
+  development of new and improved summary-index measures of global magnetic
+  disturbance related to the magnetospheric ring current and the auroral
+  electrojet, needed for validating and as input for physics-based models of the
+  space environment; and localized measures of magnetic disturbance and
+  geomagnetically-induced currents needed by the electric power-grid industry.
+  The Geomagnetism Program recently initiated a project for estimating
+  geomagnetically-induced currents in the lithosphere in order to assess and
+  mitigate hazards for the power-grid industry.
+</p>
+
