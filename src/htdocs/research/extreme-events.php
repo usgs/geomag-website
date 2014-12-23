4c87814efe43ed6e51f2b0a32a38011422dba58c
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-12-23 13:07:26 -0700

    set up extreme events page

---
+++ src/htdocs/research/extreme-events.php
@@ -0,0 +1,33 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = 'Extreme Events';
+  include 'template.inc.php';
+}
+?>
+
+<img src="/images/Lisbon-earthquake.jpg"
+    alt="Depiction of the Great Lisbon earthquake of 1755">
+
+<p>
+  Extremely large earthquakes, volcanic eruptions, magnetic storms, and other
+  hazardous geophysical events are rare. And yet their occurrence can be
+  devastating for humankind. Since direct historical records of extreme
+  geophysical events are few, the interpretation of their statistics must be
+  conducted with caution. Estimates of occurrence rates are useful, but the
+  "error-bar" measures of uncertainty are also needed.
+</p>
+
+<p>
+  Examples of extreme-event work published by USGS scientists include:
+</p>
+<ul class="referencelist">
+  <li>
+    Love, J. J., 2012.
+    <a href="http://geomag.usgs.gov/downloads/publications/2012GL051431.pdf">
+    Credible occurrence probabilities for extreme geophysical events:
+    Earthquakes, volcanic eruptions, magnetic storms</a>, Geophys. Res. Lett.,
+    39, L10301, doi:10.1029/2012GL051431
+  </li>
+</ul>
+Primary USGS contact:
+    <a href="https://profile.usgs.gov/jlove">Jeffrey J. Love</a>
