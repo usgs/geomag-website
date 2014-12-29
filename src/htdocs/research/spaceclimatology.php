d6a384c70109376973e9a973e19fb583edad2672
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-12-29 12:11:55 -0700

    created space climatology page and added image for research section

---
+++ src/htdocs/research/spaceclimatology.php
@@ -0,0 +1,77 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = 'Space Climatology';
+  include 'template.inc.php';
+}
+?>
+
+<figure class="right">
+  <img src="/images/Sun-Earth-3.jpg"
+      alt="Solar activity's effect on the Earth's magnetic field">
+</figure>
+
+<p>
+  Space climatology is concerned with longer-term changes in the space
+  environment that are driven almost entirely by changes in solar output. Data
+  from ground-based magnetic observatories, including those of the USGS
+  Geomagnetism Program, are an excellent proxy measure of near-Earth geospace
+  conditions, with continuous records going back decades and, sometimes, even
+  centuries. These data can be used to study quiet-time geomagnetic tides, and
+  decades-to-centuries-long secular change in geomagnetic disturbance and
+  magnetic-storm occurrence that is driven by solar-terrestrial interaction and
+  which is modulated by the ~11 year sunspot cycle. Results from
+  space-climatology research enhance our fundamental understanding of the Sun,
+  the Earth and the surrounding space environment; they allow us to make
+  long-term, probabilistic forecasts of space weather and magnetic storms; and
+  they provide context in studies of global climate change.
+</p>
+
+<p>
+  Examples of space-climatology work published by USGS scientists include:
+</p>
+
+<ul class="referencelist">
+  <li>
+    Love, J. J. &amp; Rigler, E. J., 2014.
+    <a href="/downloads/publications/1335.full.pdf">The magnetic tides of
+        Honolulu</a>,
+    Geophys. J. Int., 197, 1335-1353, doi:10.1093/gji/ggu090.
+  </li>
+  <li>
+    Love, J. J., Rigler, E. J. &amp; Gibson, S. E., 2012.
+    <a href="http://geomag.usgs.gov/downloads/publications/2011GL050702.pdf">
+        Geomagnetic detection of the sectorial solar magnetic field and the
+        historical peculiarity of minimum 23-24</a>,
+    Geophys. Res. Lett., 39, L04102, doi:10.1029/2012GL050702.
+  </li>
+  <li>
+    Love, J. J. &amp; Rigler, E. J., 2012.
+    <a href="http://geomag.usgs.gov/downloads/publications/2012GL051818.pdf">
+        Sunspot random walk and 22-year variation</a>,
+    Geophys. Res. Lett., 39, L10103, doi:10.1029/2012GL051818.
+  </li>
+  <li>
+    Love, J. J., Mursula, K., Tsai, V. C. &amp; Perkins, D. M., 2011.
+    <a href="http://geomag.usgs.gov/downloads/publications/2011GL049380.pdf">
+        Are secular correlations between sunspots, geomagnetic activity, and
+        global temperature significant?</a>,
+    Geophys. Res. Lett., 38, L21703, doi:10.1029/2011GL049380.
+  </li>
+  <li>
+    Love, J. J., 2011.
+    <a href="http://geomag.usgs.gov/downloads/publications/angeo-29-251-2011.pdf">
+      Secular trends in storm-level geomagnetic activity</a>,
+    Ann. Geophys., 29, 251-262.
+  </li>
+  <li>
+    Love, J. J. &amp; Gannon, J. L., 2009.
+    <a href="/downloads/publications/angeo-27-3101-2009.pdf">Revised Dst and the
+        epicycles of magnetic disturbance: 1958-2007</a>,
+    Ann. Geophys., 27, 3101-3131.
+  </li>
+</ul>
+
+<p>
+  Primary USGS contact: <a href="https://profile.usgs.gov/jlove">
+      Jeffrey J. Love</a>
+</p>
