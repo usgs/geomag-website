8e9de93b42796f6c4fad1f1f48eb72e85f0baa58
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-12-30 09:16:42 -0700

    created index page and added images for learn section

---
+++ src/htdocs/learn/index.php
@@ -0,0 +1,68 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = 'Learn';
+  $HEAD = '<link rel="stylesheet" href="index.css"/>';
+  include 'template.inc.php';
+}
+?>
+
+<div class="row">
+  <div class="one-of-two column">
+    <ul class="row no-style">
+      <li class="column">
+        <h2><a href="introtogeomag.php">Introduction to Geomagnetism</a></h2>
+        <div class="row">
+          <img src="/images/intro.png" alt=""
+              class="column mobile-one-of-four" />
+        </div>
+      </li>
+      <li class="column">
+        <h2><a href="reading.php">Further Reading</a></h2>
+        <div class="row">
+          <img src="/images/furtherreading.jpeg" alt=""
+              class="column mobile-one-of-four" />
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div class="one-of-two column">
+    <ul class="row no-style">
+      <li class="column">
+        <h2><a href="pressreleases.php">Press Releases</a></h2>
+        <div class="row">
+          <img src="/images/pressrelease.png" alt=""
+              class="column mobile-one-of-four" />
+        </div>
+      </li>
+      <li class="column">
+        <h2><a href="http://www.usgs.gov/faq/?q=taxonomy/term/9782">
+            FAQ's</a></h2>
+        <div class="row">
+          <img src="/images/faq.gif" alt=""
+              class="column mobile-one-of-four" />
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<p>
+  For centuries, navigators of the world's oceans have been familiar with an
+  effect of Earth's magnetic field: It imparts a directional preference to the
+  needle of a compass. Although in some settings magnetic orientation remains
+  important, the modern science of geomagnetism has emerged from its romantic
+  nautical origins and developed into a subject of great depth and diversity.
+  The geomagnetic field is used to explore the dynamics of Earth's interior
+  and its surrounding space environment, and geomagnetic data are used for
+  geophysical mapping, mineral exploration, risk mitigation, and other
+  practical applications. A global distribution of groundbased magnetic
+  observatories supports those pursuits by providing accurate records of the
+  magnetic-field direction and intensity at fixed locations and over long
+  periods of time.
+</p>
+
+<p>
+  Learn more:
+  <a href="/publications/comicbook/GeomagComic.pdf">Journey along a field line
+  comic book</a>.
+</p>
