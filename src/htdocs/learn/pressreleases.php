979e83263139e456d070f733ea7aba7fb8af9a4b
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-01-05 13:55:46 -0700

    cleaned up code

--- src/htdocs/learn/pressreleases.php
+++ src/htdocs/learn/pressreleases.php
@@ -15,11 +15,11 @@ if (!isset($TEMPLATE)) {
   </li>
   <li>
     10/15/2013: <a href="http://www.usgs.gov/blogs/features/usgs_top_story/the-magnetic-storm-of-halloween-2003-2/">
-        The Magnetic Storm of Halloween 2003</a>
+    The Magnetic Storm of Halloween 2003</a>
   </li>
   <li>
     5/03/2013: <a href="http://www.usgs.gov/blogs/features/usgs_top_story/does-solar-activity-cause-earthquakes/?from=title">
-        Does Solar Activity Cause Earthquakes?</a>
+    Does Solar Activity Cause Earthquakes?</a>
   </li>
 </ul>
 
@@ -28,8 +28,8 @@ if (!isset($TEMPLATE)) {
 <ul class="referencelist">
   <li>
     1/27/2012: <a href="http://www.usgs.gov/blogs/features/usgs_top_story/the-usgs-monitors-earth%E2%80%99s-magnetic-field-to-prepare-citizens-for-magnetic-storms/">
-        The USGS Monitors Earth's Magnetic Field to Prepare Citizens for
-        Magnetic Storms</a>
+    The USGS Monitors Earth's Magnetic Field to Prepare Citizens for
+    Magnetic Storms</a>
   </li>
 </ul>
 
@@ -38,6 +38,6 @@ if (!isset($TEMPLATE)) {
 <ul class="referencelist">
   <li>
     5/23/2006: <a href="http://www.usgs.gov/newsroom/article.asp?ID=1511">
-        USGS, NOAA Mark 50 Years of Geomagnetic Research at Corbin, Va.</a>
+    USGS, NOAA Mark 50 Years of Geomagnetic Research at Corbin, Va.</a>
   </li>
 </ul>
