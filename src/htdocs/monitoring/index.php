20e4bd956638da72c947d6e968add0b90b30800d
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-03-02 15:01:48 -0700

    added contact and cleaned up code

--- src/htdocs/monitoring/index.php
+++ src/htdocs/monitoring/index.php
@@ -1,50 +1,43 @@
 <?php
   if(!isset($TEMPLATE)) {
     $TITLE = 'Monitoring';
+    $CONTACT = 'geomagdata';
     include 'template.inc.php';
   }
 ?>
 
-
-  <ul class="no-style separator">
-    <li>
-      <h2><a href="operations.php">Operations</a></h2>
-      <div class="row">
-        <img src="/images/operations.jpg" alt="" class="column mobile-one-of-four" />
-        <p class="column mobile-three-of-four">
-          Learn more about the USGS Geomagnetism operations
-        </p>
-      </div>
-    </li>
-
-    <li>
-      <h2><a href="instrumentation.php">Instrumentation</a></h2>
-      <div class="row">
-        <img src="/images/instrumentation.jpg" alt=""
-            class="column mobile-one-of-four" />
-        <p class="column mobile-three-of-four">
-          See the instruments we use at our geomagnetic observatories and how
-          they help us to gather data
-        </p>
-      </div>
-    </li>
-
-    <li>
-      <h2><a href="observatories/">Observatories</a></h2>
-      <div class="row">
-        <img src="/images/observatories.jpg" alt=""
-            class="column mobile-one-of-four" />
-        <p class="column mobile-three-of-four">
-          Get information on and locations of geomagnetic observatories operated
-          by the USGS and partners of the USGS geomagnetism program
-        </p>
-      </div>
-    </li>
-
-    <h2><a href="links.php">International Observatory Programs</a></h2>
-    <p>Links to our international observatory partners' programs</p>
-
-
-
-
-  </ul>
+<ul class="no-style separator">
+  <li>
+    <h2><a href="operations.php">Operations</a></h2>
+    <div class="row">
+      <img src="/images/operations.jpg" alt="" class="column mobile-one-of-four" />
+      <p class="column mobile-three-of-four">
+        Learn more about the USGS Geomagnetism operations
+      </p>
+    </div>
+  </li>
+  <li>
+    <h2><a href="instrumentation.php">Instrumentation</a></h2>
+    <div class="row">
+      <img src="/images/instrumentation.jpg" alt=""
+          class="column mobile-one-of-four" />
+      <p class="column mobile-three-of-four">
+        See the instruments we use at our geomagnetic observatories and how
+        they help us to gather data
+      </p>
+    </div>
+  </li>
+  <li>
+    <h2><a href="observatories/">Observatories</a></h2>
+    <div class="row">
+      <img src="/images/observatories.jpg" alt=""
+          class="column mobile-one-of-four" />
+      <p class="column mobile-three-of-four">
+        Get information on and locations of geomagnetic observatories operated
+        by the USGS and partners of the USGS geomagnetism program
+      </p>
+    </div>
+  </li>
+  <h2><a href="links.php">International Observatory Programs</a></h2>
+  <p>Links to our international observatory partners' programs</p>
+</ul>
