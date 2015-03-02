33df7af1fda2a465d773dbc1c3264f152594bde4
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-03-02 14:31:42 -0700

    added contact info

--- src/htdocs/contact.php
+++ src/htdocs/contact.php
@@ -3,6 +3,7 @@
     $TEMPLATE = 'onecolumn';
     $TITLE = "Contact Us";
     $HEAD = '<link rel="stylesheet" href="contact.css"/>';
+    $CONTACT = "geomagdata";
     include 'template.inc.php';
   }
 ?>
