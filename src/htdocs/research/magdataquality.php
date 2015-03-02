24dd8caab95c5ba473f5c146000eb09bc88abf37
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-03-02 09:16:33 -0700

    img is self closing, links are site relative, escaped &

--- src/htdocs/research/magdataquality.php
+++ src/htdocs/research/magdataquality.php
@@ -1,6 +1,7 @@
 <?php
 if (!isset($TEMPLATE)) {
   $TITLE = 'Magnetic Data Quality';
+  $CONTACT = 'geomag_research';
   include 'template.inc.php';
 }
 ?>
@@ -12,7 +13,7 @@ if (!isset($TEMPLATE)) {
 </p>
 <figure>
   <img src="/images/Data.jpg"
-    alt="Observatory time series from the Halloween storm of 2003">
+    alt="Observatory time series from the Halloween storm of 2003" />
   <figcaption>
     Example analyses of magnetic data quality published by USGS scientists
     include:
@@ -21,25 +22,25 @@ if (!isset($TEMPLATE)) {
 
 <ul class="referencelist">
   <li>
-    Love, J. J., 2009. <a href="http://geomag.usgs.gov/downloads/publications/angeo-27-3601-2009.pdf">
+    Love, J. J., 2009. <a href="/downloads/publications/angeo-27-3601-2009.pdf">
     Missing data and the accuracy of magnetic observatory hour means</a>,
     Ann. Geophys., 27, 3601?3610.
   </li>
   <li>
-    Worthington, E. W., Sauter, E. A. & Love, J. J., 2009.
-    <a href="http://geomag.usgs.gov/downloads/publications/Proceedings_262-266.pdf">
+    Worthington, E. W., Sauter, E. A. &amp; Love, J. J., 2009.
+    <a href="/downloads/publications/Proceedings_262-266.pdf">
         Analysis of USGS one-second data</a>,
     <a href="http://pubs.usgs.gov/of/2009/1226/">Proc. XIII IAGA Workshop</a>,
     Golden, CO, Love, J. J. (ed.), pp. 262-266.
   </li>
   <li>
-    Love, J. J., Tsai, V. C. & Gannon, J. L., 2010.
-    <a href="http://geomag.usgs.gov/downloads/publications/angeo-28-2079-2010.pdf">
+    Love, J. J., Tsai, V. C. &amp; Gannon, J. L., 2010.
+    <a href="/downloads/publications/angeo-28-2079-2010.pdf">
         Averaging and sampling for magnetic observatory hourly data</a>,
     Ann. Geophys., 28, 2079-2096.
   </li>
   <li>
-    Love, J. J., 2011. <a href="http://geomag.usgs.gov/downloads/publications/angeo-29-1365-2011.pdf">
+    Love, J. J., 2011. <a href="/downloads/publications/angeo-29-1365-2011.pdf">
     Long-term biases in geomagnetic K and aa indices</a>, Ann. Geophys., 29,
     1365-1375.
   </li>
