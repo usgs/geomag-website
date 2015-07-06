593e81d255194d863e887f6cc2f92a88b0a6bffd
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-07-06 10:38:45 -0600

    Converted to linklist

--- src/htdocs/monitoring/index.php
+++ src/htdocs/monitoring/index.php
@@ -6,37 +6,35 @@
   }
 ?>
 
-<ul class="no-style separator">
+<ul class="linklist">
   <li>
-    <h2><a href="operations.php">Operations</a></h2>
-    <div class="row">
-      <img src="/images/operations.jpg" alt="" class="column mobile-one-of-four" />
-      <p class="column mobile-three-of-four">
-        Learn more about the USGS Geomagnetism operations
-      </p>
-    </div>
+    <a href="operations.php">
+      <h2>Operations</h2>
+      <img src="/images/operations.jpg" alt=""/>
+    </a>
+    <p>
+      Learn more about the USGS Geomagnetism operations.
+    </p>
   </li>
   <li>
-    <h2><a href="instrumentation.php">Instrumentation</a></h2>
-    <div class="row">
-      <img src="/images/instrumentation.jpg" alt=""
-          class="column mobile-one-of-four" />
-      <p class="column mobile-three-of-four">
-        See the instruments we use at our geomagnetic observatories and how
-        they help us to gather data
-      </p>
-    </div>
+    <a href="instrumentation.php">
+      <h2>Instrumentation</h2>
+      <img src="/images/instrumentation.jpg" alt=""/>
+    </a>
+    <p>
+      See the instruments we use at our geomagnetic observatories and how
+      they help us to gather data.
+    </p>
   </li>
   <li>
-    <h2><a href="observatories/">Observatories</a></h2>
-    <div class="row">
-      <img src="/images/observatories.jpg" alt=""
-          class="column mobile-one-of-four" />
-      <p class="column mobile-three-of-four">
-        Get information on and locations of geomagnetic observatories operated
-        by the USGS and partners of the USGS geomagnetism program
-      </p>
-    </div>
+    <a href="observatories/">
+      <h2>Observatories</h2>
+      <img src="/images/observatories.jpg" alt=""/>
+    </a>
+    <p>
+      Get information on and locations of geomagnetic observatories operated
+      by the USGS and partners of the USGS geomagnetism program.
+    </p>
   </li>
   <h2><a href="links.php">International Observatory Programs</a></h2>
   <p>Links to our international observatory partners' programs</p>
