ed8731b081e8c150d7a308d7b19d74b6bf31b929
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-07-06 10:08:24 -0600

    converted to linklist

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,9 +1,9 @@
 <?php
 
 if (!isset($TEMPLATE)) {
-	$TITLE = 'USGS Geomagnetism Program';
-	$HEAD = '<link rel="stylesheet" href="index.css"/>';
-	include 'template.inc.php';
+  $TITLE = 'USGS Geomagnetism Program';
+  $HEAD = '<link rel="stylesheet" href="index.css"/>';
+  include 'template.inc.php';
 }
 
 ?>
@@ -25,72 +25,68 @@ if (!isset($TEMPLATE)) {
     </div>
   </div>
   <div class="one-of-two column">
-    <ul class="row">
-      <li class="column">
-        <h2><a href="/monitoring/">Monitoring</a></h2>
-        <div class="row">
-          <img src="/images/observatory_index.jpg" alt=""
-              class="column mobile-one-of-four" />
-          <p class="column mobile-three-of-four">
-              Get information on how our magnetic observatories collect and
-              transfer data and find sources for data outside of the United
-              States
-          </p>
-        </div>
+    <ul class="linklist">
+      <li>
+        <a href="/monitoring/">
+          <h2>Monitoring</h2>
+          <img src="/images/observatory_index.jpg" alt=""/>
+        </a>
+        <p>
+          Get information on how our magnetic observatories collect and
+          transfer data and find sources for data outside of the United
+          States
+        </p>
       </li>
-      <li class="column">
-        <h2><a href="/products/">Data & Products</a></h2>
-        <div class="row">
-          <img src="/images/ring_current2_index.gif" alt=""
-              class="column mobile-one-of-four" />
-          <p class="column mobile-three-of-four">
-              View graphs of real-time magnetic data from USGS and partner
-              observatories and download historical definitive data'
-          </p>
-        </div>
+      <li>
+        <a href="/products/">
+          <h2>Data &amp; Products</h2>
+          <img src="/images/ring_current2_index.gif" alt=""/>
+        </a>
+        <p>
+          View graphs of real-time magnetic data from USGS and partner
+          observatories and download historical definitive data'
+        </p>
       </li>
-      <li class="column">
-        <h2><a href="/research/">Research</a></h2>
-        <div class="row">
-          <img src="/images/Q17_index.jpg" alt=""
-              class="column mobile-one-of-four" />
-          <p class="column mobile-three-of-four">Get information about
-              research being done by the USGS Geomagnetism Program and view
-              publications by our researchers
-          </p>
-        </div>
+      <li>
+        <a href="/research/">
+          <h2>Research</h2>
+          <img src="/images/Q17_index.jpg" alt=""/>
+        </a>
+        <p>
+          Get information about research being done by the USGS Geomagnetism
+          Program and view publications by our researchers
+        </p>
       </li>
-      <li class="column">
-        <h2><a href="/learn/">Learn</a></h2>
-        <div class="row">
-          <img src="/images/Q5_index.jpg" alt="" class="column mobile-one-of-four" />
-          <p class="column mobile-three-of-four">Learn about Earth's magnetic
-              field, space weather, magnetism and more
-          </p>
-        </div>
+      <li>
+        <a href="/learn/">
+          <h2>Learn</h2>
+          <img src="/images/Q5_index.jpg" alt=""/>
+        </a>
+        <p>
+          Learn about Earth's magnetic field, space weather, magnetism and
+          more
+        </p>
       </li>
-      <li class="column">
-        <h2><a href="/services.php">Services</a></h2>
-        <div class="row">
-          <img src="/images/MagSensors4_index.jpg" alt=""
-              class="column mobile-one-of-four" />
-          <p class="column mobile-three-of-four">
-              The USGS Geomagnetism Program provides some services such as
-              Compass Rose Surveys to governmental, academic and private
-              communities
-          </p>
-        </div>
+      <li>
+        <a href="/services.php">
+          <h2>Services</h2>
+          <img src="/images/MagSensors4_index.jpg" alt=""/>
+        </a>
+        <p>
+          The USGS Geomagnetism Program provides some services such as
+          Compass Rose Surveys to governmental, academic and private
+          communities
+        </p>
       </li>
-      <li class="column">
-        <h2><a href="/partners.php">Partners & Customers</a></h2>
-        <div class="row">
-          <img src ="/images/partners.gif" alt=""
-              class="column mobile-one-of-four" />
-          <p class="column mobile-three-of-four">
-              Meet the public and private groups and companies that contribute
-              to our program and benefit from our services
-          </p>
-        </div>
+      <li>
+        <a href="/partners.php">
+          <h2>Partners &amp; Customers</h2>
+          <img src ="/images/partners.gif" alt=""/>
+        </a>
+        <p>
+          Meet the public and private groups and companies that contribute
+          to our program and benefit from our services
+        </p>
       </li>
     </ul>
   </div>
