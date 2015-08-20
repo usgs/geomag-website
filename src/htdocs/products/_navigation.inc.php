9b0efefba8a8ade6231d154899b2714475c457e3
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-08-20 14:44:26 -0600

    changed navigation

--- src/htdocs/products/_navigation.inc.php
+++ src/htdocs/products/_navigation.inc.php
@@ -1,8 +1,8 @@
 <?php
 echo navGroup(navItem('/products/index.php', 'Data &amp; Products'),
   navItem('/products/realtime', 'Real-Time HEZF Display') .
-  navItem('/products/realtime/', 'Blended Real-Time Data - Preliminary') .
-  navItem('/products/downloads.php', 'Download') .
+  navItem('/products/realtime/', 'Blended Real-Time HEZF Display') .
+  navItem('/products/downloads.php', 'Downloads') .
   navItem('/products/storm', 'Magnetic Disturbance Events') .
   navItem('/products/models.php', 'Models') .
   navItem('/products/map', 'Real-Time Geomagnetic Conditions') .
