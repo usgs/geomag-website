fef2e45efcf975d0d669250b8494331bb1c0de20
Eric Martinez <emartinez@usgs.gov>
2015-09-10 09:28:35 -0600

    Removed viewport tag.

--- src/htdocs/monitoring/observatories/boulder/index.php
+++ src/htdocs/monitoring/observatories/boulder/index.php
@@ -1,7 +1,6 @@
 <?php
 if (!isset($TEMPLATE)) {
   $TITLE = 'Boulder (BOU)';
-  $HEAD = '<meta name="viewport" content="width=device-width">';
   $CONTACT = 'geomagdata';
   $NAVIGATION = true;
   include 'template.inc.php';
