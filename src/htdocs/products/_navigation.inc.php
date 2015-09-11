9a3911502a6e23fd4cc3b80ef77df7d1774aff76
Hal Simpson <hasimpson@usgs.gov>
2015-09-11 13:27:44 -0600

    change dst link to /realtime/dst, routing should handle it properly

--- src/htdocs/products/_navigation.inc.php
+++ src/htdocs/products/_navigation.inc.php
@@ -6,7 +6,7 @@ echo navGroup(navItem('/products/index.php', 'Data &amp; Products'),
   navItem('/products/storm', 'Magnetic Disturbance Events') .
   navItem('/products/models.php', 'Models') .
   navItem('/products/map', 'Real-Time Geomagnetic Conditions') .
-  navItem('/products/dst', 'Real-Time Dst Display') .
+  navItem('/realtime/dst', 'Real-Time Dst Display') .
   navItem('/products/otherdata.php', 'Other Sources of Data')
 );
  ?>
