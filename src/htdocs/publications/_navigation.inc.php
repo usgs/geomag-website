19f472b6720d20f2e4da9b1839190d3c443d6325
Eddie McWhirter <emcwhirter@usgs.gov>
2015-09-18 11:13:51 -0600

    Add local navigation to publications directory.

---
+++ src/htdocs/publications/_navigation.inc.php
@@ -0,0 +1,9 @@
+<?php
+echo navGroup(navItem('/publications/papers.php', 'Publications'),
+  navItem('/publications/papers.php', 'Papers') .
+  navItem('/publications/abstracts.php', 'Abstracts') .
+  navItem('/publications/charts/index.php', 'Charts') .
+  navItem('/publications/models/index.php', 'Models') .
+  navItem('/publications/movies/index.php', 'Movies')
+);
+ ?>
