ce25690ad1abb08d79b8944e25b04b31733b047f
Brown <jdbrown@igskcicgltgm083.cr.usgs.gov>
2016-09-27 09:20:36 -0600

    added all storm data as static content

---
+++ src/htdocs/storm/storm19.php
@@ -0,0 +1,109 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = '14 February 2011';
+  $HEAD = '<link rel="stylesheet" href="index.css"/>';
+  $NAVIGATION = true;
+  include 'template.inc.php';
+}
+?>
+
+<div class="column">
+  <h2>Peak Dst 30 nT</h2>
+  <p>
+    On February 14 a small magnetic storm occurred, generating a certain amount
+    of interest in the press (e.g., The Washington Post, Feb. 17, 2011). The
+    NOAA Space Weather Prediction Center, having observed a solar flare and a
+    “coronal mass ejection” from an active region in the vicinity of a sunspot
+    on Feb. 15, has forecast a 20% chance for a “minor” storm on Feb. 18. If
+    such a storm occurs, it will be measured by the USGS geomagnetic
+    observatory network, and USGS data will be used by both NOAA and the US Air
+    Force to issue warnings so that mitigating steps can be taken by satellite
+    and power-grid operators (among others). The Feb. 18 storm, if it occurs,
+    can be expected to cause aurora that can be seen at night at high latitudes.
+  </p>
+
+  <p>
+    From 2007 to about 2009, the sun was at a minimum in its 11-year cycle of
+    variation. During this time, and as a result, geomagnetic activity was
+    relatively quiescent. Now, however, we are in the midst of the early stages
+    of a new solar cycle (number 24); solar activity, sunspot numbers, and
+    geomagnetic activity have been increasing. We can expect that over the next
+    few years, and especially at solar maximum around the years 2013 and 2014,
+    that several large and potentially hazardous magnetic storms will occur.
+  </p>
+
+  <p>
+    Magnetic storms result from the dynamic interaction of the solar wind and
+    the coupled magnetospheric-ionospheric system of the Earth. Large magnetic
+    storms represent a potential hazard for the activities and infrastructure
+    of our modern, technologically based society. The possible effects of a
+    large magnetic storm were the subject of a recent National Research Council
+    report (Baker et al., 2008), which estimated that if a storm the size of
+    the 1859 one were to occur today, damage to technological systems and
+    resulting carry-on economic effects could cost the United States more than
+    $1 trillion. Such storms can cause the loss of radio communication, reduce
+    the accuracy of global-positioning systems, damage satellite electronics
+    and affect satellite operations, increase pipeline corrosion, and induce
+    voltage surges in electric power girds, causing blackouts. Even small
+    storms can sometimes have surprising effects. A “substorm” on April 5,
+    2010, resulted in the temporary loss of the Galaxy-15 communications
+    satellite that operators are still trying to repair.
+  </p>
+
+  <p>
+    Because the practical effects of space weather are realized on, or just
+    above, the surface of the Earth, ground-based sensor systems, including the
+    magnetic observatories of the USGS, play an important role in space-weather
+    monitoring (Love 2008). The intensity of the Feb. 14 event was measured by
+    the USGS storm-time disturbance index, Dst (Gannon et al., 2011), derived
+    from global observatory data, including those collected by the USGS; -Dst
+    had a maximum “main-phase” intensity of 30 nanoTesla (nT). For perspective,
+    the largest storm of the 20th that of March 1989, had a maximum intensity
+    of 525 nT, and the largest magnetic storm ever recorded, that of Sept.
+    1859, had a maximum intensity of about 1600 nT. While the Feb. 14 storm was
+    quite small, and the Feb. 18 event may not be too much larger, both are
+    reminders of what we can expect in the coming years, and the vital role of
+    the USGS magnetic observatories.
+  </p>
+
+  <ul>
+    <li>Additional Information:</li>
+    <li>
+      Real-time data from the USGS observatories, including the Dst index,
+      can be found at <a href="http://geomag.usgs.gov">http://geomag.usgs.gov
+      </a>
+    </li>
+    <li>
+      Space weather predictions can be found at
+      <a href="http://www.swpc.noaa.gov">http://www.swpc.noaa.gov</a>
+    </li>
+    <li>
+      <a href="https://www.nap.edu/catalog/12507/severe-space-weather-events-understanding-societal-and-economic-impacts-a">
+        Baker, D. N., et al.: Severe Space Weather Events -- Understanding
+        Societal and Economic Impacts, National Academy Press, Washington, DC,
+        2008.
+      </a>
+    </li>
+    <li>
+      <a href="http://pubs.usgs.gov/of/2011/1030/">
+        Gannon, J. L. et al.: U.S. Geological Survey Near Real-Time Dst Index,
+        USGS Open-File Rep. 2011-1030, 2011.
+      </a>
+    </li>
+    <li>
+      Love, J. J.: Magnetic monitoring of Earth and space, Physics Today, 61,
+      31-37, 2008.
+    </li>
+    <li>
+      <b>
+        Last Updated 2014-10-06 18:07:28	by Jeffrey J. Love and Jennifer L.
+        Gannon, USGS Geomagnetism Program, Prepared on Feb. 18, 2011 Edited by
+        Jeffrey J. Love on Sept. 15, 2014
+      </b>
+    </li>
+  </ul>
+
+  <p>
+    <a href="http://geomag.usgs.gov/map/#storm/19">View Storm Data</a>
+  </p>
+</div>
---
+++ src/htdocs/storm/storm22.php
@@ -0,0 +1,122 @@
+<?php
+if (!isset($TEMPLATE)) {
+  $TITLE = '17 March 2015';
+  $HEAD = '<link rel="stylesheet" href="index.css"/>';
+  $NAVIGATION = true;
+  include 'template.inc.php';
+}
+?>
+
+<div class="column">
+  <h2>Peak Dst -195 nT</h2>
+  <p>
+    A magnetic storm commenced on St. Patrick’s Day, Tuesday, March 17, 2015 at
+    04:45 UT with the arrival at Earth of a coronal mass ejection. The
+    geomagnetic field responded at low latitudes with a relatively strong
+    “sudden commencement” of nearly 50 nT, followed by a “main phase” and peak
+    in –Dst of almost 200 nT. The NOAA Kp index, a measure of mid-latitude
+    magnetic activity, reached a value of 8, qualifying as a severe magnetic
+    storm. More details follow.
+  </p>
+
+  <p>
+    NOAA’s Advanced Composition Explorer satellite (ACE), located on the
+    Sun-Earth line at a distance approximately 1% of the distance from the
+    Earth to the Sun, recorded an abrupt increase in solar-wind velocity from
+    an ambient value near 400 km/s to about 500 km/s. Concomitant solar-wind
+    ram pressure abruptly compressed the Earth’s magnetosphere. This generated
+    eastward-flowing magnetopause electric currents and a positive perturbation
+    in the horizontal component of the geomagnetic field recorded at
+    low-latitude magnetic observatories around the world. In Figure 1 we show
+    time series of the magnetic deviation from a quiet baseline in the
+    northward (X), eastward (Y), and down (Z) directions, as well as the
+    full-field (F), all obtained from the US Geological Survey magnetic
+    observatory in Honolulu, HI. The relatively large (~50nT) sudden
+    commencement is labeled “SC” in the figure.
+  </p>
+
+  <p>
+    After this, ACE data indicate that the solar wind’s interplanetary magnetic
+    field (IMF) was southward directed, an orientation that allows connection
+    of the IMF onto northward pointing geomagnetic field lines. Once this
+    connection occurred, the magnetosphere became strongly energetically
+    coupled with the solar wind, enhancing the westward-flowing equatorial ring
+    current of the inner magnetosphere. This process is generally recorded at
+    low-latitude magnetic observatories as a diminishment in northward
+    geomagnetic field, the longitudinal average of which is the Dst index. This
+    storm saw a maximum –Dst of 195 nT, realized on March 17 at 23:00 UT and,
+    so, by this measure this storm was the largest of this unusually subdued
+    solar cycle. By comparison, in the last solar cycle, several storms had
+    maximum –Dst exceeding 300 nT, and one exceeded 400 nT. The mid-latitude
+    magnetic activity index Kp reached a value of 8, which by NOAA space
+    weather scales qualifies as a severe storm.
+  </p>
+
+  <p>
+    As was typical for an intense storm, geomagnetic activity was particularly
+    intense at high latitudes. For example, at the Deadhorse observatory on the
+    northshore of Alaska<sup>1</sup>, magnetic declination varied by about 9
+    degrees over the course of this storm. The storm could literally be measured
+    using a simple compass! Aurora were seen across Alaska, Canada, and many
+    states in the lower continental United States. The storm attracted media
+    attention, including reports published by the Washington Post, USA Today,
+    and Christian Science Monitor.
+  </p>
+
+  <ul>
+    <li>
+      <a href="http://www.washingtonpost.com/blogs/capital-weather-gang/wp/2015/03/17/severe-geomagnetic-storm-spurs-beautiful-st-patricks-day-aurora-photos/">
+        http://www.washingtonpost.com/blogs/capital-weather-gang/wp/2015/03/17/severe-geomagnetic-storm-spurs-beautiful-st-patricks-day-aurora-photos/
+      </a>
+    </li>
+    <li>
+      <a href="http://www.usatoday.com/story/weather/2015/03/17/solar-geomagnetic-storm/24901903/">
+        http://www.usatoday.com/story/weather/2015/03/17/solar-geomagnetic-storm/24901903/
+      </a>
+    </li>
+    <li>
+      <a href="http://www.csmonitor.com/Science/2015/0319/Spectacular-aurora-photos-from-St.-Patrick-s-Day-solar-storm">
+        http://www.csmonitor.com/Science/2015/0319/Spectacular-aurora-photos-from-St.-Patrick-s-Day-solar-storm
+      </a>
+    </li>
+    <li>
+      USGS Geomagnetism Program: <a href="geomag.usgs.gov">geomag.usgs.gov</a>
+    </li>
+    <li>
+      Intermagnet data: <a href="www.intermagnet.org">www.intermagnet.org</a>
+    </li>
+    <li>
+      NOAA Space Weather Prediction Center: <a href="swpc.noaa.gov">
+          swpc.noaa.gov</a>
+    </li>
+    <li>
+      NOAA Space Weather forecasts:
+      <a href="http://www.swpc.noaa.gov/forecasts">
+        http://www.swpc.noaa.gov/forecasts
+      </a>
+    </li>
+    <li>
+      NOAA Space Weather forecast archive:
+      <a href="ftp://ftp.swpc.noaa.gov/pub/warehouse/2015">
+        ftp://ftp.swpc.noaa.gov/pub/warehouse/2015
+      </a>
+    </li>
+  </ul>
+  <hr />
+  <ul class="referencelist">
+    <li>
+      1. [The Deadhorse observatory is operated as a public-private partnership
+      between the USGS and Schlumberger. Data from the observatory are used for
+      directional drilling for oil and gas; the data are also freely available
+      to the wider scientific community.]
+    </li>
+  </ul>
+  <p>
+    <b>
+      Last Updated 2015-06-25 15:13:16	by Jeffrey J. Love and E. Joshua Rigler
+    </b>
+  </p>
+  <p>
+    <a href="http://geomag.usgs.gov/map/#storm/22">View Storm Data</a>
+  </p>
+</div>
