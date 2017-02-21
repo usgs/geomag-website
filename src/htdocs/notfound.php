3eaff8e0591fefced4c44105ebac1cff2900051b
lisa-usgs <lisa@usgs.gov>
2017-02-21 16:38:40 -0700

    edited notfound file

--- src/htdocs/notfound.php
+++ src/htdocs/notfound.php
@@ -34,8 +34,7 @@
   </li>
   <li>
     <p>
-      If you still can't find what you're looking for, <strong><a href="https://answers.usgs.gov/cgi-bin/gsanswers">Contact Us</a></strong> for assistance.  Select the Topic <strong>&ldquo;Natural Hazards&rdquo;</strong>, and please include the <strong>URL</strong> of the webpage in question in your message if relevant. Also, tell us if you clicked on the link from another website.<br/>
-      (Example URL: https://geomag.usgs.gov/path-to-webpage/)
+      If you still can't find what you're looking for, <strong><a href="https://answers.usgs.gov/cgi-bin/gsanswers">Contact Us</a></strong> for assistance.  Select the Topic <strong>&ldquo;Natural Hazards&rdquo;</strong>, and please include the <strong>URL</strong> of the webpage in question in your message if relevant. Also, tell us if you clicked on the link from another website.
     </p>
   </li>
 </ul>
