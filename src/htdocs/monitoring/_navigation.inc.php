83b0f281c103586dcb06277abd092d53cb7176fc
lisa-usgs <lisa@usgs.gov>
2018-10-25 16:15:18 -0600

    removing unneeded files after migration to Drupal

--- src/htdocs/monitoring/_navigation.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo navGroup(navItem('/monitoring/index.php', 'Monitoring'),
-  navItem('/monitoring/operations.php', 'Operations') .
-  navItem('/monitoring/instrumentation.php', 'Instrumentation') .
-  navItem('/monitoring/observatories', 'Observatories') .
-  navItem('/monitoring/links.php', 'International Observatory Programs')
-);
- ?>
