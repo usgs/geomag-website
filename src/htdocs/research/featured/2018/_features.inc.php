353c5b1b69d27137b1a4a38250e3077332256420
lisa-usgs <lisa@usgs.gov>
2018-11-06 10:52:50 -0700

    added links to web article

--- src/htdocs/research/featured/2018/_features.inc.php
+++ src/htdocs/research/featured/2018/_features.inc.php
@@ -11,6 +11,16 @@ $GM_FEATURES->author = 'U.S. Geological Survey';
 $GM_FEATURES->siteUrl = 'https://geomag.usgs.gov/';
 $GM_FEATURES->baseUrl = '';
 
+$GM_FEATURES->items[] = array(
+   'id' => '2018-04-09',
+   'title' => 'Keeping the Lights On in North America',
+   'content' => 'Realtime geoelectric maps during a magnetic storm can assist utility companies with their operations and can help power-grid managers to make decisions that may minimize the impact to their systems.',
+   'link' => 'https://geomag.usgs.gov/research/featured/2018/geoelect-maps/',
+   'modified' => strtotime('2018-04-09'),
+   'thumbnail' => 'https://geomag.usgs.gov/research/featured/2018/geoelect-maps/images/US-lights-night.jpg',
+   'image' => 'https://geomag.usgs.gov/research/featured/2018/geoelect-maps/images/US-lights-night.jpg'
+ );
+
  $GM_FEATURES->items[] = array(
     'id' => '2018-04-09',
     'title' => 'The Electric Storm of November 1882',
